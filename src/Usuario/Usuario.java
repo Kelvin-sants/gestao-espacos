734ed2bd20371dd0ff4b53507612c9aef1576d5f
Kelvin-sants <kelvinvini75@gmail.com>
2025-07-24 21:40:16 -0300

    feat: adicionando novas funcionalidades em Usuario.java
    Adicionando novas funcionalidades em Usuario.java do package Usuario

--- src/Usuario/Usuario.java
+++ src/Usuario/Usuario.java
@@ -1,13 +1,18 @@
 package Usuario;
+import Autenticacao.SenhaUtils;
 
 public class Usuario{
     private String nome;
+    private String email;
+    private String senhaHash;
     private boolean isAdm;
 
     //metodo construtor
-    public Usuario(String nome){
+    public Usuario(String nome, String email, String senha){
         this.nome = nome;
         this.isAdm = false;
+        this.email = email;
+        this.senhaHash = SenhaUtils.gerarHash(senha);
     }
 
     //getters
@@ -20,6 +25,14 @@ public class Usuario{
         return this.isAdm;
     }
 
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getSenhaHash(){
+        return this.senhaHash;
+    }
+
     //setters
 
     public void setNome(String nome){
@@ -28,5 +41,13 @@ public class Usuario{
 
     public void setStatusAdm(boolean status){
         this.isAdm = status;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setSenhaHash(String senhaHash){
+        this.senhaHash = senhaHash;
     }
 }
